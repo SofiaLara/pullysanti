42d653b860150447acac33bdb56cd9fe932bf26b
Sofia Peyret <sofiapeyret@gmail.com>
2023-09-18 14:37:49 +0100

    add path for img render

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Hero = () => {
     return (
         <>
-            <Image alt="pull y santi intro image" src="/img_intro_bnw2.JPG" priority fill={true} style={{ objectFit: "cover", zIndex: 0 }} />
+            <Image alt="pull y santi intro image" src="./img_intro_bnw2.JPG" priority fill={true} style={{ objectFit: "cover", zIndex: 0 }} unoptimized/>
             <h1 className='font-sans'>pull & santi</h1>
             <div className='intro-line'></div>
             <p className='fecha font-sans'>Te invitamos a <br/> nuestro casamiento <br/> 28.10.2023</p>
