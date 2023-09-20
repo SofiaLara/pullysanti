06132dcdac217998eada6415463e2c320159e97c
Sofia Peyret <sofiapeyret@gmail.com>
2023-09-20 22:56:10 +0100

    foto portada

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Hero = () => {
     return (
         <>
-            <Image alt="pull y santi intro image" src="./foto-portada-2.jpg" priority fill={true} style={{ objectFit: "cover", zIndex: 0 }} unoptimized/>
+            <Image alt="pull y santi intro image" src="./foto-portada-web.jpg" priority fill={true} style={{ objectFit: "cover", zIndex: 0 }} unoptimized/>
             <h1>Pull <Image className='inline-block' alt="y image" src="./logo__footer.svg" unoptimized width='50' height='50'/> Santi</h1>
             {/* <p className='fecha'>28.10.2023</p> */}
             <Image className='fecha' alt="bouquet image" src="./bouquet.png" unoptimized width='100' height='100'/>
